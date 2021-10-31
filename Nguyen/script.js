47f3690c1402ed6390986dabc8c50e9989bbd647
Tony Le <nguyenle.tonytech@gmail.com>
2021-10-31 20:30:41 +0700

    Fix sidebar and report page

--- Nguyen/script.js
+++ Nguyen/script.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   var trigger = $(".hamburger"),
     overlay = $(".overlay"),
     isClosed = false;
-
   trigger.click(function () {
     hamburger_cross();
   });
@@ -10,13 +9,9 @@ $(document).ready(function () {
   function hamburger_cross() {
     if (isClosed == true) {
       overlay.hide();
-      trigger.removeClass("is-open");
-      trigger.addClass("is-closed");
       isClosed = false;
     } else {
       overlay.show();
-      trigger.removeClass("is-closed");
-      trigger.addClass("is-open");
       isClosed = true;
     }
   }
